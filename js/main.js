f207e79894f1ae2521e0dfa00438aa3607737a5f
Valentina Iglesias <valentinaiglesiasg@hotmail.com>
2016-07-18 08:51:09 -0400

    trabajo de peliculas, falta arreglar el inicio y la informacion

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).on('click',function () {
+	$('#container').mixItUp();
+});
+
+$(document).ready(function(){ 
+   $('#info').on('click',function(){
+      $('#mostrar').toggle('slow');
+   });
+});
