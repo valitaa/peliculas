e5b3babec37368502f46946b7288c12392f14ffb
Valentina Iglesias <valentinaiglesiasg@hotmail.com>
2016-07-18 09:24:51 -0400

    arreglo de fondo

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
-$(document).on('click',function () {
-	$('#container').mixItUp();
-});
-
 $(document).ready(function(){ 
    $('#info').on('click',function(){
       $('#mostrar').toggle('slow');
    });
 });
+$(document).on('click',function () {
+	$('#container').mixItUp();
+});
+
+
